714c2f4bdd2da6e74fb8ff4ffdec0ae9074e0023
Stevenvr <vanroyesteven@gmail.com>
2018-07-21 23:50:04 +0200

    Add Toolbar and close button

--- app/src/main/java/com/example/steven/drinkpicker/AddDrinkBACActivity.java
+++ app/src/main/java/com/example/steven/drinkpicker/AddDrinkBACActivity.java
@@ -2,6 +2,9 @@ package com.example.steven.drinkpicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AddDrinkBACActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class AddDrinkBACActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_drink_bac);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_bac);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.baseline_close_white_24);
+        setTitle(R.string.add_drink_bac_activity);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == android.R.id.home) {
+            onBackPressed();
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "BACK BUTTON", Toast.LENGTH_SHORT).show();
+        super.onBackPressed();
+
     }
 }
